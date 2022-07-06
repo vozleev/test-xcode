fcdea6ef92092d30388503c3d333371d3beaa959
Yury Vozleev <yury@vozleev.ru>
2022-07-06 21:32:02 +0300

    Initial commit

---
+++ xcode-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xcode-test
+//
+//  Created by Yury Vozleev on 06.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xcode-test/xcode_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_testApp.swift
+//  xcode-test
+//
+//  Created by Yury Vozleev on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct xcode_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
